8ff43a855c131436e60ca2d97e808df44f71e3d7
rbadwar <rbadwar@IN-G6FD9G3>
2022-11-15 13:26:24 +0530

    Add project files.

---
+++ Day3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to moriningstar");
+            
+            Console.ReadLine();
+
+        }
+    }
+}
